3b735b59794bcd6a621b6e0b5932f4d00056b896
jauri <jauricardenas18@gmail.com>
2025-09-14 12:31:11 -0500

    print arrays

---
+++ One_and_Two.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    //itera por cada caso
+
+    int t; //numero de casos
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+
+        //itera para cada elemento
+        int n; //numero de elementos
+        cin >> n;
+
+        int arrElementos[n]//array para guardar los elementos
+        for (int j = 0; j < n; j++) {
+            
+            int k; //elemento
+            cin >> k;
+
+            arrElementos[j] = k
+        }
+    }
+}
