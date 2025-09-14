cf0e7df75fc979054f3664f6b7578ed0570001e0
jauri <jauricardenas18@gmail.com>
2025-09-14 16:39:52 -0500

    codigo casi funcionando

--- One_and_Two.cpp
+++ One_and_Two.cpp
@@ -2,25 +2,76 @@
 #include <vector>
 
 using namespace std;
+int comparar_productos(int k, int n, int arrElementos[]);
+int get_posicion(int k, int n, int arrElementos[]);
 
 int main() {
-    //itera por cada caso
-
+    
     int t; //numero de casos
     cin >> t;
+    //itera por cada caso
     for (int i = 0; i < t; i++) {
 
-        //itera para cada elemento
         int n; //numero de elementos
         cin >> n;
 
-        int arrElementos[n]//array para guardar los elementos
+        int arrElementos[n];//array para guardar los elementos
+
+        //itera por cada elemento y lo guarda en una array
         for (int j = 0; j < n; j++) {
             
-            int k; //elemento
-            cin >> k;
+            int elem; //elemento
+            cin >> elem;
 
-            arrElementos[j] = k
+            arrElementos[j] = elem;
         }
+
+        //ANALIZA EL ARRAY Y DETECTA DONDE SON IGUALES SUS PRODUCTOS
+
+        //devuelve la posicion en la que los dos productos son iguales
+        //sino detecta igualdad devuelve -1 y avanza a la siguiente posicion
+        bool found_k = false;
+        for (int k = 1; k < n; k++) {
+
+            int igualdad = comparar_productos(k, n, arrElementos);
+            //si detecta igualdad imprime la posicion 
+            if (igualdad) {
+                //al detectar una igualdad no sigue buscando
+
+                found_k = true;
+                cout << k << endl;
+                break;
+            }
+        }
+        
+        //si se encontró una k va al siguiente caso
+        if (found_k) {
+            continue;
+        }
+        else {
+            //si no encuentra ninguna igualdad devuelve -1 y sigue con su vida
+            cout << -1 << endl;
+        }
+
     }
+}
+
+int comparar_productos(int k, int n, int arrElementos[]) {
+    
+    //producto del lado izquierdo
+    int prodIzq = 1;
+    for (int izq = 0; izq < k; izq++) {
+        //obtener numeros del lado izquierdo
+        prodIzq = prodIzq * arrElementos[izq];
+    }
+    
+    //producto del lado derecho
+    int prodDer = 1;
+    for (int der = k; der < n; der++) {
+        //obtener numeros del lado derecho
+        prodDer = prodDer * arrElementos[der];
+    }
+
+    return (prodIzq == prodDer);
+    
 }
